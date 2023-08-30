3b850fb321ff39bd478ff30aa96d69ffc6255e99
CultENOT <132144566+CultENOT@users.noreply.github.com>
2023-08-30 13:56:29 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let result = ''
+  const processedN = n.toString()
+  const nLenght = processedN.length
+  for (let i = 0; i < nLenght; i++) {
+    if (processedN[i] == '-') {
+      i++
+    }
+    result = `${processedN[i]}${result}`
+  }
+  return result
 }
